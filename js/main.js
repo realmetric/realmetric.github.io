e34db7c631561ee4113c860729c91393280dfb97
Andrey Reinwald <andreinwald@gmail.com>
2017-02-18 13:57:35 +0200

    Added Vue.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-new Vue({
-    el: '#app',
-    data: {
-        message: 'Client'
-    }
-})
+window.onload = function () {
+
+    var router = new VueRouter({mode: 'history'});
+
+    new Vue({
+        router: router,
+        el: '#metrics',
+        data: {
+            metrics: []
+        },
+        created: function () {
+            this.$http.get('http://' + this.$route.query.api + '/metrics').then(function (response) {
+                this.metrics = response.body;
+            });
+        }
+    });
+}
